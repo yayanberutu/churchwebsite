cb31569dccbd8fb9bee42499890c63cb785fa570
yayanberutu <yoseprib@gmail.com>
2026-05-16 22:55:32 +0700

    fix: modify default value for table content on db migration

--- backend/migrations/004_update_announcements_table.sql
+++ backend/migrations/004_update_announcements_table.sql
@@ -1,7 +1,7 @@
 -- backend/migrations/004_update_announcements_table.sql
 
 -- Add content column to announcements
-ALTER TABLE announcements ADD COLUMN content TEXT NOT NULL DEFAULT '';
+ALTER TABLE announcements ADD COLUMN content TEXT NULL;
 
 -- Create announcement_attachments table
 CREATE TABLE IF NOT EXISTS announcement_attachments (
